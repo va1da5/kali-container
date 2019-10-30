47d4f9f1c730173e2e1fa6b137c8d3199fc61680
Vaidas <vaidas.vaiciunas@inbox.lt>
2019-10-30 23:08:07 +0200

    Updates packages, aliases and build script

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-echo "[!] Starting Kali image build process"
-docker build -t $USER/kali-docker files \
-    && echo "[+] Kali images was built successfully"
+g="\e[32m"
+e="\e[0m"
 
-echo "[?] Do you want to access run.sh from anywhere in the system as 'kali' command? [Y/N]"
+echo -e "$g[!]$e Starting Kali image build process"
+docker build -t $USER/kali-docker files \
+    && echo -e "$g[+]$e Kali images was built successfully"
+
+echo -e "$g[?]$e Do you want to access run.sh from anywhere in the system as 'kali' command? [Y/N]"
 read input
 
 if [ "${input}" == "Y" ] || [ "${input}" == "y" ]
 then
-    sudo ln -s "$PWD/run.sh" "/usr/local/bin/kali"
-    echo "[+] Installed! Now you use 'kali' command in any directory"
+    sudo ln -s "$PWD/run.sh" "/usr/local/bin/kali" \
+        && echo -e "$g[+]$e Installed! Now you use 'kali' command in any directory"
 fi
